042fbad79c893bfcb6c78e17944113cd663cbd9f
holger-hellinger-ext_sonepar <holger.hellinger_ext@sonepar.com>
2026-01-21 10:44:35 +0100

    config: Add passthrough copy for .htaccess, robots.txt, and site.webmanifest to _site root

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
 module.exports = function (eleventyConfig) {
   // Passthrough copy for assets (CSS, Images)
   eleventyConfig.addPassthroughCopy("src/assets");
+  
+  // Passthrough copy for root-level files
+  eleventyConfig.addPassthroughCopy("src/.htaccess");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
+  eleventyConfig.addPassthroughCopy("src/site.webmanifest");
 
   return {
     dir: {
